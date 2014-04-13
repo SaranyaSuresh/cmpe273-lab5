4a85d07a6d541732fb889703d5cdf66367e91eb5
SaranyaSuresh <suresaranya@gmail.com>
2014-04-12 17:39:35 -0700

    Lab 5 commit

--- library/src/main/java/edu/sjsu/cmpe/library/ui/views/HomeView.java
+++ library/src/main/java/edu/sjsu/cmpe/library/ui/views/HomeView.java
@@ -3,8 +3,9 @@ package edu.sjsu.cmpe.library.ui.views;
 import com.yammer.dropwizard.views.View;
 
 import edu.sjsu.cmpe.library.domain.Book;
+import java.util.List;
 
-public class HomeView extends View {
+/*public class HomeView extends View {
     private final Book book;
 
     public HomeView(Book book) {
@@ -14,5 +15,19 @@ public class HomeView extends View {
 
     public Book getBook() {
 	return book;
+    }
+}*/
+
+public class HomeView extends View 
+{
+    private final List<Book> books;
+    public HomeView(List<Book> books) 
+    {
+              super("home.mustache");
+              this.books = books;
+    }
+    public List<Book> getBooks() 
+    {
+              return books;
     }
 }
--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,14 @@
 $(":button").click(function() {
 	var isbn = this.id;
 	alert('About to report lost on ISBN ' + isbn);
+	$.ajax({
+		url: "/library/v1/books/"+isbn+"?status=lost",
+		type: 'PUT',
+		success: function() 
+		{
+			$('#status-'+isbn).html("lost");
+			$('#'+isbn).attr("disabled","disabled");
+		}	
+	});
 });
+
